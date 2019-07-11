bb19523b0e45f4d3f07f8491d5e2e45b16fdd10f
محمد دريبي <437105909@student.ksu.edu.sa>
2019-07-11 14:45:56 +0300

    Product Manager has been built and tested

---
+++ Myshop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using Myshop.Core.Models;
+
+namespace Myshop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+        string Message = "";
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+           
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+            Message = "Saved into List";
+        }
+
+        public bool Add(Product p)
+        {
+            products.Add(p);
+            Message = "Added Successfully!";
+            return true;
+        }
+
+        public bool Update(Product p)
+        {
+            Product updatedProduct = products.Find(a => a.Id == p.Id);
+            if (updatedProduct != null)
+            {
+                updatedProduct = p;
+                Message = "Updated Successfully...";
+                return true;
+            }
+            else
+            {
+                Message = "Product can not be found there!";
+                throw new Exception("Product can not be found there!");
+            }
+        }
+
+        public Product Find(string Id)
+        {
+            Product product = products.Find(a => a.Id == Id);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("This Id can not be found");
+            }
+        }
+
+        public IQueryable<Product> CollectionOfProducts()
+        {
+            return products.AsQueryable();
+        }
+
+        public bool Delete(string Id)
+        {
+            Product deletedProduct = products.Find(a => a.Id == Id);
+            if (deletedProduct != null)
+            {
+                products.Remove(deletedProduct);
+                Message = "Product Removed Successfully";
+                return true;
+            }
+            else
+            {
+                throw new Exception("There is no Product to Remove!");
+            }
+        }
+
+    }
+}
---
+++ Myshop.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,134 @@
+﻿using Myshop.DataAccess.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Myshop.Core.Models;
+namespace Myshop.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+
+        ProductRepository context;
+
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
+
+
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<Product> product = context.CollectionOfProducts().ToList();
+            return View(product);
+            
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(p);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(p);
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            Product p = context.Find(Id);
+
+            if (p != null)
+            {
+                return View(p);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product p, string Id)
+        {
+            Product product = context.Find(Id);
+
+            if(product != null )
+            {
+                if (ModelState.IsValid)
+                {
+                    product.Name = p.Name;
+                    product.Category = p.Category;
+                    product.Description = p.Description;
+                    product.Image = p.Image;
+                    product.Price = p.Price;
+
+                    context.Commit();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+
+        }
+
+        
+        public ActionResult Delete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+            if (productToDelete != null)
+            {
+                return View(productToDelete);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+            if (ModelState.IsValid)
+            {
+                if (productToDelete != null)
+                {
+                    context.Delete(Id);
+                    context.Commit();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
